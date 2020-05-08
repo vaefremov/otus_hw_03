273508aa8e344f84580b8ae77db37df2b23be57b
Vladimir Efremov <efremov@Vladimir.local>
2020-05-09 00:32:25 +0200

    Recursion converted to loop in order to ensure that the compaler uses inline

--- include/utilities.h
+++ include/utilities.h
@@ -12,7 +12,14 @@ inline int factorial(int n) {
     {
         throw std::invalid_argument("Factorial: argument is too large");
     }
-    return (n < 1) ? 1 : factorial(n - 1) * n;
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        res *= i;
+    }
+    
+    // return (n < 1) ? 1 : factorial(n - 1) * n;
+    return res;
 }
 
 }
